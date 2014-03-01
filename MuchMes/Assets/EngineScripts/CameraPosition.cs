c6fb691cc6434d9e74b60907680fcf088d836e1b
Adam <adamgryu@gmail.com>
2014-03-01 00:08:20 -0500

    world go

---
+++ MuchMes/Assets/EngineScripts/CameraPosition.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPosition {
+
+	public static CameraPosition instance = new CameraPosition(Camera.main);
+
+	Camera camera;
+
+	public CameraPosition(Camera camera) {
+		this.camera = camera;
+	}
+
+	public Vector3 topLeft {
+		get {
+			Vector3 position = Camera.main.ScreenToWorldPoint (new Vector3 (0, Camera.main.pixelHeight, 10));
+			position.z = 0;
+			return position;
+		}
+	}
+}
---
+++ MuchMes/Assets/Scripts/CameraAnchor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class CameraAnchor : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = CameraPosition.instance.topLeft;
+
+		GameObject backPlane = this.transform.FindChild("BackPlane").gameObject;
+		if (backPlane != null) {
+			// Standard Unity plane is 10x10.
+			Vector3 scale = Vector3.one * ((1f / 10f) * Camera.main.orthographicSize * 2);
+			scale.x *= Camera.main.aspect;
+			backPlane.transform.localScale = scale;
+
+			backPlane.transform.localPosition = 10 * new Vector3(scale.x, -scale.y, 0) / 2f;
+			//Camera.main.orthographicSize
+		}
+	}
+}
