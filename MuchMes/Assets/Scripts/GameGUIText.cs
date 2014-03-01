53fb745e4872431709f55c169d52adec2a1a4034
Adam <adamgryu@gmail.com>
2014-03-01 15:01:07 -0500

    ASDAD

--- MuchMes/Assets/Scripts/GameGUIText.cs
+++ MuchMes/Assets/Scripts/GameGUIText.cs
@@ -1,18 +1,25 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 
 public class GameGUIText : MonoBehaviour {
 
 	private TextMesh textMesh;
+	private Action<GameGUIText> onClick;
 
 	// Use this for initialization
 	void Start () {
-		this.textMesh = this.GetComponent<TextMesh>(); 
+		this.textMesh = this.GetComponentInChildren<TextMesh>(); 
 	}
 
 	// Update is called once per frame
 	void Update () {
-	
+
+	}
+
+	void OnMouseDown() {
+		if (onClick != null)
+			onClick(this);
 	}
 
 	public void SetText(string text) {
--- MuchMes/Assets/Scripts/QuizMenu/QuizController.cs
+++ MuchMes/Assets/Scripts/QuizMenu/QuizController.cs
@@ -1,17 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System;
 
 public class QuizController : MonoBehaviour {
 
+	public GameGUIText buttonPrefab;
+
+	List<String> questionList = new List<string>() {"make him sad", "give up!", "the flavour is too much!!!"};
+
 	GameGUIText header;
+	GameObject questions;
 
 	// Use this for initialization
 	void Start () {
-	
+		this.header = GameObject.Find("GameGUIHeader").GetComponent<GameGUIText>();
+
+
+		foreach (String s in questionList) {
+
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 }
