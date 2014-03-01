198b2f30951c94629a92d4c2f1c3843f70f4b794
akbiggs <alexanderkbiggs@gmail.com>
2014-02-28 20:46:26 -0500

    Script that requests a server

--- MuchMes/Assets/Scripts/RequestServer.cs
+++ MuchMes/Assets/Scripts/RequestServer.cs
@@ -1,15 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Net;
+using System.IO;
+using System.Text;
 
-public class NewBehaviourScript : MonoBehaviour {
-
+public class RequestServer : MonoBehaviour {
 	// Use this for initialization
-	void Start () {
-	
+	void Start() {
+		HttpWebRequest request = (HttpWebRequest)WebRequest.Create ("http://www.timeapi.org/utc/now");
+		HttpWebResponse response = (HttpWebResponse)request.GetResponse ();
+		Stream receiveStream = response.GetResponseStream();
+		StreamReader reader = new StreamReader(receiveStream, Encoding.UTF8);
+
+		Debug.Log("Read response");
+		Debug.Log("Response is: " + reader.ReadToEnd());
+
+		response.Close();
+		reader.Close();
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-	
+	void Update() {
 	}
 }
